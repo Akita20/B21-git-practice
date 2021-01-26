1dd3d09c4f3973acf9da177a3c5b972a628469d2
Akita20 <jamila.osmn@gmail.com>
2021-01-26 12:16:41 -0800

    movie 1 branch updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        System.out.println("Hello World B21");
         System.out.println("B21 future SDET learning Git");
 
     }
